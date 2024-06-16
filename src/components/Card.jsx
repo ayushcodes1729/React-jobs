8ad7b3c989f901ba3fdb8425db9612f04579ff87
Ayush Kumar <ayushtiwari0803@gmail.com>
2024-06-16 17:01:51 +0530

    Content wrapper added to first cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Card({ children }) {
+export default function Card({ children, bg="bg-gray-100" }) {
     return (
-        <div className=' bg-gray-100'>
+        <div className={`${bg} p-6 rounded-lg shadow-md`}>
             {children}
-        </div>
-    )
-}
+        </ div>
+            )
+        }
