8d9d5eeaea7fa6dcd1467b7ed642a4a0d3fe5d90
Ayush Kumar <ayushtiwari0803@gmail.com>
2024-06-17 12:02:43 +0530

    Showing recent 3 jobs at home

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -3,7 +3,7 @@ import Jobs from '../jobs.json'
 import JobListing from './JobListing';
 
 export default function JobListings() {
-    console.log(Jobs);
+    const recentJobs = Jobs.slice(0,3);
     return (
         <section className="bg-blue-50 px-4 py-10">
             <div className="container-xl lg:container m-auto">
@@ -11,7 +11,7 @@ export default function JobListings() {
                     Browse Jobs
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {Jobs.map((job) => (
+                    {recentJobs.map((job) => (
                         <JobListing key={job.id} job={job}/>
                     ))}
 
