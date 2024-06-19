0bfff9ab189f7e5f009e88f0974717010f638436
Ayush Kumar <ayushtiwari0803@gmail.com>
2024-06-19 17:43:10 +0530

    Backend Removed

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,27 +1,15 @@
 import React from 'react'
+import Jobs from '../jobs.json'
 import { useState , useEffect } from 'react';
+
 import JobListing from './JobListing';
 import Spinner from './Spinner';
 
 export default function JobListings({isHome = false}) {
-    const [jobs, setJobs] = useState([]);
-    const [loading, setLoading] = useState(true);
+    // const [jobs, setJobs] = useState([]);
+    // const [loading, setLoading] = useState(true);
 
-    useEffect (()=>{
-        const fetchJobs = async ()=>{
-            const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs'
-            try {
-                const res = await fetch(apiUrl);
-                const data =await res.json();
-                setJobs(data);
-            } catch (error) {
-                console.log("Error fetching the Data", error);
-            }finally {
-                setLoading(false)
-            }
-        }
-        fetchJobs();
-    }, [])
+    const recentJobs = isHome ? Jobs.slice(0,3) : Jobs;
     return (
         <section className="bg-blue-50 px-4 py-10">
             <div className="container-xl lg:container m-auto">
@@ -29,8 +17,17 @@ export default function JobListings({isHome = false}) {
                     {isHome ? 'Recent Jobs' : 'Browse Jobs'}
                 </h2>
                 
-                    {loading ? (<Spinner loading={loading}/>) : (<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                        {jobs.map((job) => (
+                {/* Future Opportunities */}
+                    {/* {loading ? (<Spinner loading={loading}/>) : (<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {recentJobs.map((job) => (
+                            <JobListing key={job.id} job={job}/>
+                        ))}
+                    </div>
+                        
+                    )} */}  
+
+                    {(<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {recentJobs.map((job) => (
                             <JobListing key={job.id} job={job}/>
                         ))}
                     </div>
